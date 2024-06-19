8fdbce172c6fc9b445f6b807ef79c7beb9a3c6ec
Buns Enchantress <bunsthedev@gmail.com>
2024-06-19 11:22:12 -0500

    update: site config

--- packages/frontend/src/config/site.tsx
+++ packages/frontend/src/config/site.tsx
@@ -1,12 +1,15 @@
 export const siteConfig = {
-  title: 'Chainlink Functions Demo dApp',
-  description: 'Chainlink Functions Demo dApp',
+  title: 'RWA Tokenization | Chainlink Functions',
+  description: 'Chainlink Functions example for tokenizing real-world assets',
   links: {
     github: 'https://github.com/BunsDev/rwa-tokenization',
     docs: 'https://dev.chain.link/products/functions',
     functionsDocs:
       'https://docs.chain.link/chainlink-functions/getting-started',
   },
+  image: 'https://chateau.voyage/opengraph-image.png',
+  ogImage: 'https://chateau.voyage/opengraph-image.png',
+  twitterImage: 'https://chateau.voyage/opengraph-image.png',
 }
 
 export type SiteConfig = typeof siteConfig
